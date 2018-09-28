9c17e52d59a25d2ede6bfa0f2a9bb36525a3c5bd
Garrett Moore <GMoore@nwadmin.com>
2018-09-28 16:11:27 -0700

    Started smooth scrolling

--- website.js
+++ website.js
@@ -1,38 +1,21 @@
 /*Javascript to do
 -smooth page scroll
--social bar change color on hover
--portfolio icons size increase on hover
--borders glow on hover
 -Header text transition in on page load
-
-//-Create const link to header underline
-
-const titleUnderline = document.getElementById("titleUnderline");
-
-
-function addGlow(){
-    titleUnderline.classList.add("glow");
-    setTimeout(function(){titleUnderline.classList.remove("glow")}, 1000);
-    console.log("hover");
-}
-
-function main(){
-    titleUnderline.addEventListener("mouseover", addGlow());
-}
-
-main();
-
+*/
+const scrollOffset = 20;
 const blog = document.getElementById("blogBookmark");
 const blogLink = document.getElementById("blogLink");
 
-function scrollToBlog() {
-    blogBookmark.scrollIntoView({
-        behavior: "smooth"
-    })
+
+function blogScroll(){
+    blog.scrollIntoView({ 
+        behavior: 'smooth' 
+    });
 };
 
-function main(){
-    blogLink.addEventListener("click", scrollToBlog());
-}
 
-main();*/
+function main(){
+    blogLink.addEventListener("click", blogScroll());
+};
+
+main();
