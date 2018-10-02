4489285caa0682dd655ecfd17c0d386921983b0d
Garrett Moore <GMoore@nwadmin.com>
2018-10-02 14:54:11 -0700

    Smooth scrolling added

--- website.js
+++ website.js
@@ -1,21 +1,3 @@
 /*Javascript to do
--smooth page scroll
 -Header text transition in on page load
 */
-const scrollOffset = 20;
-const blog = document.getElementById("blogBookmark");
-const blogLink = document.getElementById("blogLink");
-
-
-function blogScroll(){
-    blog.scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-};
-
-
-function main(){
-    blogLink.addEventListener("click", blogScroll());
-};
-
-main();
