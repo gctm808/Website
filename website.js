7ffb85cee1f6c4af81b97a74b9ff2ffb74110505
Garrett Moore <GMoore@nwadmin.com>
2018-10-03 16:59:11 -0700

    Resizing, header animations

--- website.js
+++ website.js
@@ -1,3 +1,17 @@
 /*Javascript to do
 -Header text transition in on page load
 */
+
+function remove(){
+    document.body.classList.remove('fade-out');
+    document.getElementById("line1").classList.remove("titleFade");
+    document.getElementById("line2").classList.remove("titleFade");
+    document.getElementById("line3").classList.remove("titleFade");
+    document.getElementById("titleUnderline").classList.remove("titleFade");
+}
+
+function main(){
+    remove();
+};
+
+main();
